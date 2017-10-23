3265100a51d81d88c6489201afe3b50e0e689345
Imrik <pierre.mar.66000@gmail.com>
2017-10-23 10:54:46 +0200

    reverse adding

--- main.js
+++ main.js
@@ -23,25 +23,46 @@ $("a").click(function(e){
     //On recupere la page de destination
     var $target = $("[data-page='"+target+"']");
 
-    //On déplace la page de destination a droite de la page en cours
-    $current.after( $target );
+    //On check si le reverse est actif
+    var reverse = $(this).data("dir") == "reverse";
 
-    //On affiche la page de destination
-    $target.show().addClass("active");
+    if( reverse ){
+        //On place l'élément cible derriere l'élément actif
+        $current.before( $target );
 
-    //Je selectionne mes 2 éléments
-    $entities = $current.add( $target );
+        //On attribut la classe active a notre élément
+        $target.show().addClass("active");
 
-    //J'anime la transition
-    $entities.addClass("animation");
+        //On ajoute la classe reversed pour le css aux deux éléments
+        $entities = $current.add( $target );
+        $entities.addClass("reversed");
+
+        //On déclenche l'animation avec quelque centieme de retard
+        setTimeout(function(){
+            $entities.addClass("animation-reverse");
+        }, 0);
+    }
+    else{
+        //On déplace la page de destination a droite de la page en cours
+        $current.after( $target );
+
+        //On affiche la page de destination
+        $target.show().addClass("active");
+
+        //Je selectionne mes 2 éléments
+        $entities = $current.add( $target );
+
+        //J'anime la transition
+        $entities.addClass("animation");
+    }
 
     //Le temps correspond au temps de la css class animation
     setTimeout(function(){
         //Je cache l'ancienne page active
         $current.hide().removeClass("active");
 
-        //Je retire toute les classes animations
-        $entities.removeClass("animation");
+        //Je retire toute les classes animations et reverse
+        $entities.removeClass("animation reversed animation-reverse");
         
     }, 500);
 
